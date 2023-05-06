260e0a973b3898ea1d6d62adbfb1864ba82185df
Alfie Richardson <01richardsonalfie@protonmail.com>
2023-05-06 05:45:44 +0100

    page for posts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>My Site - @yield('title')</title>
+    </head>
+
+    <body>
+        <h1>My Site - @yield('title')</h1>
+        
+        <div>
+            @yield('content')
+        </div>
+    </body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Posts')
+
+@section('content')
+    <p>This is a list of posts made on the site</p>
+@endsection
